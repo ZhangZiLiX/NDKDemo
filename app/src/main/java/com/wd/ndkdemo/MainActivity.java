8fe666b0b6873e08a84996bf2393c1cd7030c3a4
ZhangZiLi <2649040899@qq.com>
2019-04-22 21:58:09 +0800

    提交

---
+++ app/src/main/java/com/wd/ndkdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.wd.ndkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    // 此处设置了一个按钮用于触发JNI方法
+    private Button Button;
+
+    // 步骤1:加载生成的so库文件
+    // 注意要跟.so库文件名相同
+    static {
+
+        System.loadLibrary("hello_jni");
+    }
+
+    // 步骤2:定义在JNI中实现的方法
+    public native String getFromJNI();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 通过Button调用JNI中的方法
+        Button = (Button) findViewById(R.id.button);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button.setText(getFromJNI());
+            }
+        });
+    }
+}
---
+++ app/src/main/jni/test.cpp
@@ -0,0 +1,14 @@
+
+# include <jni.h>
+# include <stdio.h>
+
+extern "C"
+{
+
+    JNIEXPORT jstring JNICALL Java_com.wd.ndkdemo_getFromJNI(JNIEnv *env, jobject obj ){
+       // 参数说明
+       // 1. JNIEnv：代表了VM里面的环境，本地的代码可以通过该参数与Java代码进行操作
+       // 2. obj：定义JNI方法的类的一个本地引用（this）
+    return env -> NewStringUTF("Hello!");
+    }
+}
